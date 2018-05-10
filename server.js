67a8d056189776c83985cc20fdeb0e7d4c3eede5
mguldberg <guldberg@sbcglobal.net>
2018-05-10 15:28:53 -0500

    deployment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { Promise } from "mongoose";
+
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -15,6 +17,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
+);
 
 // Add routes, both API and view
 app.use(routes);
@@ -24,11 +31,6 @@ app.use(routes);
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
-);
 
 // Start the API server
 app.listen(PORT, function () {
